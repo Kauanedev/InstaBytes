fd23d13cb8e61aaad9c2b8bd7f906c095627ede2
Kauane Santos <kauanesantos.coding@gmail.com>
2024-11-21 17:27:37 -0300

    feat: create server + router

---
+++ src/router.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const router = express()
+
+router.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+
+
+export default router
---
+++ src/server.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import router from './router.js';
+
+dotenv.config();
+const port = process.env.PORT
+
+const app = express()
+
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+})
+
+app.use(router)
+
