921ce9f71d6bc464dd7f37c60ae77ba75a0bd7f5
Kauane Santos <kauanesantos.coding@gmail.com>
2024-11-22 15:53:24 -0300

    feat: get posts from mongo atlas

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,12 +1,26 @@
+import connectToDatabase from '../config/database.js'
 import posts from '../postsDb.js'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const conexao = await connectToDatabase(process.env.MONGO_DB)
+
+export async function getPostsFromDb() {
+    const db = conexao.db("imersao-instabytes")
+    const collection = db.collection("posts")
+
+    return await collection.find().toArray()
+}
 
 export async function GetPosts(req, res) {
     try {
+        const posts = await getPostsFromDb()
+
         return res.status(200).json(posts)
     } catch (error) {
         return res.status(500).json({error: error})
     }
-
 }
 
 function findPostById(id) {
--- src/server.js
+++ src/server.js
@@ -1,14 +1,11 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import router from './router.js';
-import conectarAoBanco from './config/database.js';
 
 dotenv.config();
 const port = process.env.PORT
 
 const app = express()
-
-await conectarAoBanco(process.env.MONGO_DB)
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
