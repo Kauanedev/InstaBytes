3e2cc2d3fe6dff6a1affb859c30ac8789d2f8967
Kauane Santos <kauanesantos.coding@gmail.com>
2024-11-26 11:46:59 -0300

    refactor: change prompt + clean gemini response

--- src/services/geminiService.js
+++ src/services/geminiService.js
@@ -7,8 +7,8 @@ const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"});
 
 export default async function generateDescriptionWithGemini(imageBuffer) {
     const prompt = [
-        "Gere uma descrição em português do brasil para a seguinte imagem",
-        "Gere apenas uma opção de uma descrição concisa para acessibilidade para a seguinte imagem em português do Brasil.",
+        "Gere uma descrição em português do brasil para a seguinte imagem, em apenas uma linha",
+        "Gere apenas uma opção de uma descrição concisa para acessibilidade para a seguinte imagem em português do Brasil, em apenas uma linha.",
     ];
 
     try {
@@ -24,37 +24,15 @@ export default async function generateDescriptionWithGemini(imageBuffer) {
         const fullDescription = fullDescriptionResponse.response.text();
         const fullAltText = fullAltTextResponse.response.text();
 
-        const selectedDescription = selectDescription(fullDescription, 1);
-        const selectedAltText = extractConciseAltText(fullAltText);
+        const cleanDescription = fullDescription.replace(/\n/g, "");
+        const cleanAltText = fullAltText.replace(/\n/g, "");
 
         return {
-            description: selectedDescription || "Alt-text não disponível.",
-            alt: selectedAltText || "Alt-text não disponível.",
+            description: cleanDescription || "",
+            alt: cleanAltText || "",
         };
     } catch (error) {
         console.error("Erro ao obter alt-text:", error.message, error);
         throw new Error("Erro ao obter o alt-text do Gemini.");
     }
-}
-
-function selectDescription(fullDescription, optionNumber) {
-    const regex = new RegExp(`\\*\\*Opção ${optionNumber}.*?\\*\\*\\n\\n>(.*?)\\n`, 's');
-    const match = fullDescription.match(regex);
-
-    if (match && match[1]) {
-        return match[1].trim();
-    }
-    return null;
-}
-
-function extractConciseAltText(fullAltText) {
-    const regex = /"([^\"]+)"/;
-
-    const match = fullAltText.match(regex);
-
-    if (match && match[1]) {
-        return match[1].trim();
-    }
-
-    return null;
 }
